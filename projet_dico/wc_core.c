a473be1cfafbd1a7e0ea7722ba76193ae5711bef
xavier.dubarry <xavier.dubarry@ensta.fr>
2026-06-02 11:26:44 +0200

    Initial commit

---
+++ projet_dico/wc_core.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define isspace(c) (c==' ' || c=='\t' || c=='\n' || c=='\f' || c=='\r')
+
+long lcount;			/* Count of lines */
+long wcount;			/* Count of words */
+long ccount;			/* Count of characters */
+
+long ltotal;			/* Total count of lines */
+long wtotal;			/* Total count of words */
+long ctotal;			/* Total count of characters */
+
+void count(FILE* f);
+
+void count(FILE* f)
+{
+  register int c;
+  register int word = 0;
+
+  lcount = 0;
+  wcount = 0;
+  ccount = 0L;
+
+  while ((c = getc(f)) != EOF) {
+	ccount++;
+
+	if (isspace(c)) {
+		if (word) wcount++;
+		word = 0;
+	} else {
+		word = 1;
+	}
+
+	if (c == '\n' || c == '\f') lcount++;
+  }
+  ltotal += lcount;
+  wtotal += wcount;
+  ctotal += ccount;
+}
